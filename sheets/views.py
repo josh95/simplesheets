3ac81e5384d2505fc029b91b31d0685f50faf30f
Josh <jjr95@cornell.edu>
2018-04-05 20:41:14 -0400

    Charater sheet editor designed

--- sheets/views.py
+++ sheets/views.py
@@ -18,7 +18,8 @@ def sheetMain(request):
         character_sheets.append({
             "name": sheet.character_name,
             "image" : sheet.img_url,
-            "description" : sheet.description})
+            "description" : sheet.description,
+            "id": sheet.sheet_id})
             
     context["sheets"] = character_sheets
     context["player"] = player_name
@@ -29,14 +30,28 @@ def index(request):
     return render(request, "login.html")
 
 def sheetEditor(request):
-    context = {}
+    try:
+        player_id = request.session['player_id']
+    except:
+        return redirect("../sheetMain")
 
-    return render(request, "charsheet.html", context)
+    sheet_id = request.GET.get('sheetID')
+    
+    context = {"playerID" :player_id}
+    if sheet_id is None:
+        #new sheet
+        return render(request, "charsheet.html", context)
+    else:
+        sheet = Sheets.objects.get(sheet_id = sheet_id)
+        context['sheetID'] = sheet.sheet_id
+        context['charName'] = sheet.character_name
+        context['imgurl'] = sheet.img_url
+        context['description'] = sheet.description
+        return render(request, "charsheet.html", context)
 
 def checkUser(request):
     try:
         player_id = Player.objects.get(player_name = request.POST['user']).player_id
-        print(player_id)
     except:
         pass
     return HttpResponse(player_id, content_type="application/json")
